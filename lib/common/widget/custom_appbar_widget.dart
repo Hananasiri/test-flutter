1c2e3381d5eea8617cad99df0e5b53d019524fc1
Hanan <91421012+Hananasiri@users.noreply.github.com>
2023-12-24 11:05:49 +0300

    Initial commit

---
+++ lib/common/widget/custom_appbar_widget.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final BuildContext screenContext;
+  final List<Widget>? actions;
+
+  const CustomAppBar({super.key, this.actions, required this.screenContext});
+
+  @override
+  Size get preferredSize => const Size.fromHeight(60);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: const Text('title'),
+      actions: actions,
+    );
+  }
+}
